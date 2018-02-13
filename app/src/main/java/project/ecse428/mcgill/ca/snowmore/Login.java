958e5d154f92f0c6480158f32e841cc8fcb1fdbb
Nimitkumar Nagar <nimitnagar@gmail.com>
2018-02-13 16:51:39 -0500

    Login Firebase - Committed By Shamil and Nimit

--- app/src/main/java/project/ecse428/mcgill/ca/snowmore/Login.java
+++ app/src/main/java/project/ecse428/mcgill/ca/snowmore/Login.java
@@ -4,11 +4,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,11 +19,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import backend.LoginBackend;
 
 
 
 public class Login extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
 
     private EditText email_login;
     private EditText password_login;
@@ -33,7 +43,7 @@ public class Login extends AppCompatActivity {
     private LoginBackend loginBackend;
     private Dialog dialog = null;
     private Context context = null;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +54,14 @@ public class Login extends AppCompatActivity {
         context = Login.this;
         setUpVariables();
     }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        //updateUI(currentUser);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,39 +100,71 @@ public class Login extends AppCompatActivity {
 
     //Sign In button action
     public void signInButton(View view) {
+            login();
+        Intent login = new Intent(this , WelcomePage.class);
+        startActivity(login);
 
-        if(TextUtils.isEmpty(email_login.getText().toString())) {
-            error_message_email_login.setText("Please enter email");
-            error_message_email_login.setVisibility(View.VISIBLE);
-        }
-        else {
-            if(!loginBackend.check_email_login(email_login.getText().toString())) {
-                error_message_email_login.setText("Invalid email");
-                error_message_email_login.setVisibility(View.VISIBLE);
-            }
-            else {
-                error_message_email_login.setVisibility(View.INVISIBLE);
-            }
-        }
-        if(TextUtils.isEmpty(password_login.getText().toString())) {
-            error_message_password_login.setText("Please enter password");
-            error_message_password_login.setVisibility(View.VISIBLE);;
-        }
-        else {
-            if(!loginBackend.check_password_login((password_login.getText().toString()))) {
-                error_message_password_login.setText("Invalid password");
-                error_message_password_login.setVisibility(View.VISIBLE);
-            }
-            else {
-                error_message_password_login.setVisibility(View.INVISIBLE);
-            }
-        }
+//        if(TextUtils.isEmpty(email_login.getText().toString())) {
+//            error_message_email_login.setText("Please enter email");
+//            error_message_email_login.setVisibility(View.VISIBLE);
+//        }
+//        else {
+//            if(!loginBackend.check_email_login(email_login.getText().toString())) {
+//                error_message_email_login.setText("Invalid email");
+//                error_message_email_login.setVisibility(View.VISIBLE);
+//            }
+//            else {
+//                error_message_email_login.setVisibility(View.INVISIBLE);
+//            }
+//        }
+//        if(TextUtils.isEmpty(password_login.getText().toString())) {
+//            error_message_password_login.setText("Please enter password");
+//            error_message_password_login.setVisibility(View.VISIBLE);;
+//        }
+//        else {
+//            if(!loginBackend.check_password_login((password_login.getText().toString()))) {
+//                error_message_password_login.setText("Invalid password");
+//                error_message_password_login.setVisibility(View.VISIBLE);
+//            }
+//            else {
+//                error_message_password_login.setVisibility(View.INVISIBLE);
+//            }
+//        }
+//
+//        if(loginBackend.check_email_login(email_login.getText().toString()) && loginBackend.check_password_login((password_login.getText().toString()))) {
+//            login();
+//            Toast toast = Toast.makeText(context, "Successfully Logged In", Toast.LENGTH_SHORT);
+//            toast.show();
+//
+//        }
 
-        if(loginBackend.check_email_login(email_login.getText().toString()) && loginBackend.check_password_login((password_login.getText().toString()))) {
-            Toast toast = Toast.makeText(context, "Successfully Logged In", Toast.LENGTH_SHORT);
-            toast.show();
-        }
+    }
+    private void login() {
+        mAuth.signInWithEmailAndPassword(email_login.getText().toString(), password_login.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
 
+//                        // [START_EXCLUDE]
+//                        if (!task.isSuccessful()) {
+//                            mStatusTextView.setText(R.string.auth_failed);
+//                        }
+//                        hideProgressDialog();
+//                        // [END_EXCLUDE]END_EXCLUDE
+                    }
+                });
     }
 
     //Registration button action
