9361acbfc18c44609455d61f6827e648e272ee28
karimb94 <karim.el-baba@mail.mcgill.ca>
2018-02-13 19:23:09 -0500

    User data base logout added

--- app/src/main/java/backend/LogOutButton.java
+++ app/src/main/java/backend/LogOutButton.java
@@ -6,11 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import project.ecse428.mcgill.ca.snowmore.Login;
 
 public class LogOutButton extends AppCompatActivity {
 
     private static LogOutButton logout= new LogOutButton();
+    private FirebaseAuth mAuth;
 
     public static void logOut(Button logOut){
         logOut.setOnClickListener(new View.OnClickListener(){
@@ -22,6 +26,9 @@ public class LogOutButton extends AppCompatActivity {
     }
     private void logOutFunction(){
         Intent intent = new Intent(this, project.ecse428.mcgill.ca.snowmore.Login.class);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signOut();
         startActivity(intent);
     }
 }
