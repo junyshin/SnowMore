52f108a95d26cadd712e26b5313bab5e3d6a58a6
lucien-george <lucien.george@mail.mcgill.ca>
2018-02-20 19:48:00 -0500

    request display

--- app/src/main/java/project/ecse428/mcgill/ca/snowmore/ClientShovelerPage.java
+++ app/src/main/java/project/ecse428/mcgill/ca/snowmore/ClientShovelerPage.java
@@ -1,6 +1,7 @@
 package project.ecse428.mcgill.ca.snowmore;
 
 import android.app.Dialog;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by junshin on 2018-02-14.
@@ -28,6 +37,7 @@ public class ClientShovelerPage extends AppCompatActivity {
     private DatabaseReference myRef;
     private Firebase mRootRef;
     private String postID;
+    private TextView city;
 
     private Dialog dialog = null;
     private Context context = null;
@@ -36,10 +46,21 @@ public class ClientShovelerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_shoveler_page);
-
+        setUpVariables();
         mAuth = FirebaseAuth.getInstance();
         myFirebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = myFirebaseDatabase.getReference();
+        myRef = myFirebaseDatabase.getReference().child("requestPost");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                collectData((Map<String,Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // handle database error
+            }
+        });
         //FirebaseUser user = mAuth.getCurrentUser();
         //userID = user.getUid();
 
@@ -55,6 +76,10 @@ public class ClientShovelerPage extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_user_registration, menu);
         return true;
+    }
+
+    public void setUpVariables() {
+        city = (TextView) findViewById(R.id.city);
     }
 
     @Override
@@ -91,5 +116,47 @@ public class ClientShovelerPage extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mAuth.signOut();
         startActivity(intent);
+    }
+
+    // details of request button
+    public void detailsButton(View view) {
+    }
+
+    private void collectData(Map<String,Object> requestInfo) {
+
+        ArrayList<String> address = new ArrayList<>();
+        ArrayList<String> city = new ArrayList<>();
+        ArrayList<String> phoneNumbers = new ArrayList<>();
+        ArrayList<String> postalCode = new ArrayList<>();
+
+        //iterate through each post request, ignoring their UID
+        for (Map.Entry<String, Object> entry : requestInfo.entrySet()){
+
+            Map singleUser = (Map) entry.getValue();
+            address.add((String) singleUser.get("address"));
+        }
+
+        //iterate through each post request, ignoring their UID
+        for (Map.Entry<String, Object> entry : requestInfo.entrySet()){
+
+            Map singleUser = (Map) entry.getValue();
+            city.add((String) singleUser.get("city"));
+        }
+
+        //iterate through each post request, ignoring their UID
+        for (Map.Entry<String, Object> entry : requestInfo.entrySet()){
+
+            Map singleUser = (Map) entry.getValue();
+            phoneNumbers.add((String) singleUser.get("phone"));
+        }
+
+        //iterate through each post request, ignoring their UID
+        for (Map.Entry<String, Object> entry : requestInfo.entrySet()){
+
+            Map singleUser = (Map) entry.getValue();
+            postalCode.add((String) singleUser.get("postalCode"));
+        }
+
+//        System.out.println(phoneNumbers.toString());
     }
 }
