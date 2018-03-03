eb1df5a24330cff518df923b26554d40f6c543e0
mikym2 <maatouk.michael@gmail.com>
2018-03-03 09:52:13 -0500

    Valid Test for shoveling request trial

--- app/src/test/java/project/ecse428/mcgill/ca/snowmore/ShovelingRequestTest.java
+++ app/src/test/java/project/ecse428/mcgill/ca/snowmore/ShovelingRequestTest.java
@@ -1,9 +1,31 @@
 package project.ecse428.mcgill.ca.snowmore;
 
+import android.view.View;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Michael Maatouk on 2/22/2018.
  */
 public class ShovelingRequestTest {
+
+    @Test
+    public void postRequestTest(){
+
+        View view = null;
+        UserShovelingRequest userShovelingRequest = new UserShovelingRequest();
+        userShovelingRequest.streetAddress = null;
+        userShovelingRequest.error_message_streetAddress = null;
+        userShovelingRequest.streetAddress.setText("Saint-Catherine");
+      //  Bundle bundle = new Bundle();
+       // userShovelingRequest.onCreate(bundle);
+
+        userShovelingRequest.postRequest(view);
+
+        assertEquals(userShovelingRequest.error_message_streetAddress.getText(), "Please enter street address");
+    }
 
 
 }
